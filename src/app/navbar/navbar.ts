47bdebf3478b3481536ff6e0fcc4f5d1b6189330
miler2 <vegavegewtu@gmail.com>
2026-04-13 20:35:39 +0200

    Navbar is completely finished and functional

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -15,31 +15,48 @@ export class Navbar {
 
   setSelectedPageToLandPage() {
     this.selectedPage = 'landPage';
+    this.hamburgerClose();
   }
 
   setSelectedPageToAngular() {
     this.selectedPage = 'angular';
+    this.hamburgerClose();
   }
 
   setSelectedPageToRedes() {
     this.selectedPage = 'redes';
+    this.hamburgerClose();
+  }
+
+
+  // HAMBURGER
+  hamburgerActiveToggle() {
+    document.getElementById('nav-links')?.classList.toggle('open');
+  }
+
+  hamburgerClose() { 
+    document.getElementById('nav-links')?.classList.remove('open');
   }
 
 
   // CV DROPDOWN SELECTION
   setSelectedCvToAngularEs() {
     this.cv.setSelectedCvToAngularEs();
+    this.hamburgerClose();
   }
 
   setSelectedCvToRedesEs() {
     this.cv.setSelectedCvToRedesEs();
+    this.hamburgerClose();
   }
 
   setSelectedCvToAngularEn() {
     this.cv.setSelectedCvToAngularEn();
+    this.hamburgerClose();
   }
 
   setSelectedCvToRedesEn() {
     this.cv.setSelectedCvToRedesEn();
+    this.hamburgerClose();
   }
 }
