29b1de50c954b278b2b7fc01b82d1d75b1181088
miler2 <vegavegewtu@gmail.com>
2026-04-17 20:59:05 +0200

    I have finished the first iteration of the website

--- src/app/pages/redes-sistemas/redes-sistemas.ts
+++ src/app/pages/redes-sistemas/redes-sistemas.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../footer/footer";
+import { CvService } from '../cv/cv-service';
+import { Routing } from '../../navbar/routing';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-redes-sistemas',
@@ -7,4 +10,19 @@ import { Footer } from "../../footer/footer";
   templateUrl: './redes-sistemas.html',
   styleUrl: './redes-sistemas.scss',
 })
-export class RedesSistemas {}
+export class RedesSistemas {
+  constructor(private cv: CvService, private routing: Routing, private router: Router){}
+  
+  setSelectedCvToRedesEs(){
+    this.cv.setSelectedCvToRedesEs();
+  }
+
+  setSelectedCvToRedesEn(){
+    this.cv.setSelectedCvToRedesEn();
+  }
+
+  setSelectedPageToAngular() {
+    this.routing.setSelectedPageToAngular();
+    this.router.navigate(['angular']);
+  }
+}
