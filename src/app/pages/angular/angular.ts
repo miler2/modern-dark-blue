51bac5a0b34db19e95c5e126f4c15f71b46c7b84
miler2 <vegavegewtu@gmail.com>
2026-04-21 13:56:55 +0200

    I have corrected all links of the website with href

--- src/app/pages/angular/angular.ts
+++ src/app/pages/angular/angular.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../footer/footer";
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-angular',
-  imports: [Footer],
+  imports: [Footer, RouterLink],
   templateUrl: './angular.html',
   styleUrl: './angular.scss',
 })
-export class Angular {
-  constructor(private router: Router){}
-
-  goToCvAngularEs(){
-    this.router.navigate(['cv_angular_espanol.pdf']);
-  }
-
-  goToCvAngularEn(){
-    this.router.navigate(['cv_angular_english.pdf']);
-  }
-}
+export class Angular {}
--- src/app/pages/redes-sistemas/redes-sistemas.ts
+++ src/app/pages/redes-sistemas/redes-sistemas.ts
@@ -1,27 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../footer/footer";
-import { Routing } from '../../navbar/routing';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-redes-sistemas',
-  imports: [Footer],
+  imports: [Footer, RouterLink],
   templateUrl: './redes-sistemas.html',
   styleUrl: './redes-sistemas.scss',
 })
-export class RedesSistemas {
-  constructor(private routing: Routing, private router: Router){}
-  
-  setSelectedCvToRedesEs(){
-    this.router.navigate(['cv_english.pdf']);
-  }
-
-  setSelectedCvToRedesEn(){
-    this.router.navigate(['cv_english.pdf']);
-  }
-
-  setSelectedPageToAngular() {
-    this.routing.setSelectedPageToAngular();
-    this.router.navigate(['angular']);
-  }
-}
+export class RedesSistemas {}
